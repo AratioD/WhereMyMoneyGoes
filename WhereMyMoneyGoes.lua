dc648bb9fd7b29c46b7e2de970d85056143da62b
Antti Heroja <antti.heroja@kone.com>
2019-11-24 21:08:51 +0200

    now I can parse files, but not very well

--- WhereMyMoneyGoes.lua
+++ WhereMyMoneyGoes.lua
@@ -1,25 +1,27 @@
---  local accountFile = require "tiliTiedot.csv"
--- -- local m = accountFile = require "tiliTiedot.csv"
--- -- .read('tiliTiedot.csv') -- read file csv1.txt to matrix m
--- -- print(m[2][3])                       -- display element in row 2 column 3 (102)
--- -- -- m[1][3] = 'changed'                  -- change element in row 1 column 3
--- -- -- m[2][3] = 123.45                     -- change element in row 2 column 3
--- -- -- csvfile.write('./csv2.txt', m)    
-
--- local citylist = {}
--- for line in io.lines(accountFile) do
---     local city, region, coalition, coordinate_x, coordinate_y = line:match("%s*(.-),%s*(.-),%s*(.-),%s*(.-),%s*(.-)")
---     citylist[#citylist + 1] = { city = city, region = region, coalition = coalition, coordinate_x = coordinate_x, coordinate_y = coordinate_y }
--- end
 local open = io.open
 
-local function read_file(path)
+local function readFile(path)
     local file = open(path, "rb") -- r read mode and b binary mode
-    if not file then return nil end
+    if not file then
+        return nil
+    end
     local content = file:read "*a" -- *a or *all reads the whole file
+    --print(content)
     file:close()
     return content
 end
 
-local fileContent = read_file("tiliTiedot.csv");
-print (fileContent);
+local function lineByLine(file)
+    for i in string.gmatch(file, "%S+") do
+        if string.match(i, "Oyj") then
+            print(i)
+            print("enterprise found")
+        else
+            print("The word tiger was not found.")
+        end
+    end
+end
+
+local fileContent = readFile("tiliTiedot.csv")
+--print(fileContent)
+lineByLine(fileContent)
