98bf6a2e738e8161778bd6c9f91b12f418c5917d
AratioD <antti.heroja@kone.com>
2019-12-04 22:34:19 +0200

    sorting is working now

--- WhereMyMoneyGoes.lua
+++ WhereMyMoneyGoes.lua
@@ -97,6 +97,37 @@ for i = 1, lengthOflist, 1 do
 end
 
 --lengthOflist = #accountTransfer
+-- table.sort(accountTransfer)
 
+-- for key,value in pairs(accountTransfer) do print(key,value) end
 
-for key,value in pairs(accountTransfer) do print(key,value) end
+local function spairs(t, order)
+    -- collect the keys
+    local keys = {}
+    for k in pairs(t) do keys[#keys+1] = k end
+
+    -- if order function given, sort by it by passing the table and keys a, b,
+    -- otherwise just sort the keys 
+    if order then
+        table.sort(keys, function(a,b) return order(t, a, b) end)
+    else
+        table.sort(keys)
+    end
+
+    -- return the iterator function
+    local i = 0
+    return function()
+        i = i + 1
+        if keys[i] then
+            return keys[i], t[keys[i]]
+        end
+    end
+end
+
+-- for k,v in spairs(accountTransfer) do
+--     print(k,v)
+-- end
+
+for k,v in spairs(accountTransfer, function(t,a,b) return t[b] < t[a] end) do
+    print(k,v)
+end
